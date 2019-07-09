caa3f57154af7175323af4c08e29b3b02e5bb960
Mr-Bowtie <mr.bowtie989@gmail.com>
2019-07-08 21:36:43 -0500

    file name change and test of locally stored credentials

---
+++ exercises/small_problems/easy_3/palindromes.rb
@@ -0,0 +1,24 @@
+# input: string
+# output: boolean true or false
+# requires: method to factor spaces, punctuation, and capitalization into palindrome
+
+#this method works for both strings and arrays. 
+def palindrome?(str)
+  str.reverse == str
+end
+
+def real_palindrome?(str)
+  string = str.downcase.delete('^a-z0-9')
+  palindrome?(string)
+  string
+end 
+
+# puts real_palindrome?("Madam i'm adam")
+
+def palindromic_number?(num)
+  num = num.to_s
+  palindrome?(num)
+end
+
+puts palindromic_number?(34543)
+puts palindromic_number?(123454)
