5c3ab79e5782d7cc9536f8bf809b5676602e5f4a
Mr-Bowtie <mr.bowtie989@gmail.com>
2020-10-23 00:37:28 -0500

    deleted old project, added final bonus feature to new project

--- RB101/lesson_2/rpslsp.rb
+++ RB101/lesson_2/rpslsp.rb
@@ -1,10 +1,34 @@
 require "pry"
+REDO_ANSWERS = ["yes", "y"]
 VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
 VALID_ABBREVIATION = ["r", "p", "s", "l", "sp"]
-PLAYS = { "rock" => ["scissors", "lizard"], "paper" => ["rock", "spock"], "scissors" => ["paper", "lizard"], "spock" => ["scissors", "rock"], "lizard" => ["spock", "paper"] }
+PLAYS = { "rock" => ["scissors", "lizard"],
+          "paper" => ["rock", "spock"],
+          "scissors" => ["paper", "lizard"],
+          "spock" => ["scissors", "rock"],
+          "lizard" => ["spock", "paper"] }
+WINNING_SCORE = 5
 
 def prompt(message)
   puts "=> #{message}"
+end
+
+def display_choices
+  prompt("choose one: #{VALID_CHOICES.join(", ")}")
+  prompt("or: #{VALID_ABBREVIATION.join(", ")}")
+end
+
+def get_user_input
+  loop do
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      return choice
+    elsif VALID_ABBREVIATION.include?(choice)
+      return convert_user_choice(choice)
+    else
+      prompt "Invalid input: refer to choices above"
+    end
+  end
 end
 
 def valid_choice?(input)
@@ -12,33 +36,26 @@ def valid_choice?(input)
 end
 
 def convert_user_choice(input)
-  if input.length <= 2
-    case input
-    when "r"
-      input = "rock"
-    when "p"
-      input = "paper"
-    when "s"
-      input = "scissors"
-    when "l"
-      input = "lizard"
-    when "sp"
-      input = "spock"
-    end
+  case input
+  when "r"
+    input << "ock"
+  when "p"
+    input << "aper"
+  when "s"
+    input << "cissors"
+  when "l"
+    input << "izard"
+  when "sp"
+    input << "pock"
   end
-  input
 end
 
 def win?(first, second)
-  if PLAYS[first].include?(second)
-    return true
-  end
+  PLAYS[first].include?(second)
 end
 
 def lose?(first, second)
-  if PLAYS[second].include?(first)
-    return true
-  end
+  PLAYS[second].include?(first)
 end
 
 def display_result(player, computer)
@@ -51,28 +68,53 @@ def display_result(player, computer)
   end
 end
 
-loop do
-  choice = ""
-  loop do
-    prompt("choose one: #{VALID_CHOICES.join(", ")}")
-    prompt("or: #{VALID_ABBREVIATION.join(", ")}")
-    choice = gets.chomp
+def display_play(player, computer)
+  prompt "You chose #{player} : Computer chose #{computer}"
+end
 
-    break if valid_choice?(choice)
-    prompt("That's not a valid choice.")
+def display_score(player, comp)
+  prompt "Player #{player} : Computer #{comp} \n"
+end
+
+def redo?
+  redo_message()
+  REDO_ANSWERS.include?(gets.chomp)
+end
+
+def exit_message
+  prompt "Thank you for playing. Goodbye!"
+end
+
+def display_match_winner(player_score)
+  if player_score == 5
+    prompt "You have won the match"
+  else
+    prompt "The Computer has won the match"
   end
-  choice = convert_user_choice(choice)
+end
 
-  computer_choice = VALID_CHOICES.sample
+def redo_message
+  prompt "Would you like to play again?"
+end
 
-  # binding.pry
-  display_result(choice, computer_choice)
+loop do
+  player_score = 0
+  comp_score = 0
+  until player_score == WINNING_SCORE || comp_score == WINNING_SCORE
+    display_choices()
+    choice = get_user_input()
+    system("clear") || system("cls")
+    computer_choice = VALID_CHOICES.sample
 
-  puts "You chose #{choice}: computer chose #{computer_choice}"
+    player_score += 1 if win?(choice, computer_choice)
+    comp_score += 1 if lose?(choice, computer_choice)
 
-  prompt "Do you want to play again?"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?("y")
+    display_result(choice, computer_choice)
+    display_play(choice, computer_choice)
+    display_score(player_score, comp_score)
+  end
+  display_match_winner(player_score)
+  break unless redo?()
 end
 
 prompt "Thank you for playing! Good bye"
