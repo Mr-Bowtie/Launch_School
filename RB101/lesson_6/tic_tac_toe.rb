dd691e57e5706d57a6564347ad96041ce2837e5d
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-05-04 01:59:04 -0500

    added improved join and score keeping

--- RB101/lesson_6/tic_tac_toe.rb
+++ RB101/lesson_6/tic_tac_toe.rb
@@ -1,17 +1,9 @@
-# input: player input of position of next play
-# output: a board with the players position choice and the computers, outcome of the game if it has met certain criteria
-# rules:
-#   1.display empty board
-#   2. player inputs move
-#   3. computer generates a move
-#   4. updated board is displayed
-#   5. if winner display winner
-#   6. if board full, display tie
-#   7. if neither, back to 2
-#   8. if game over, ask player to play again, if yes back to 1
-#   9. display exit message
 require "pry"
 require "pry-byebug"
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                [[1, 5, 9], [3, 5, 7]]
+
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
@@ -20,6 +12,18 @@ def prompt(input)
   puts "==> #{input}"
 end
 
+def joinor(arr, delimiter = ", ", word = "or")
+  case arr.size
+  when 0 then ""
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system "clear"
   puts " Player = #{PLAYER_MARKER} Computer = #{COMPUTER_MARKER}"
@@ -38,10 +42,24 @@ def display_board(brd)
   puts ""
 end
 
+# rubocop:enable Metrics/AbcSize
+
 def initialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+def display_score(player_score, comp_score)
+  prompt "SCORE - Player: #{player_score} Computer: #{comp_score}"
+end
+
+def display_match_winner(player_score, comp_score)
+  if player_score == 5
+    prompt "Player wins the match!"
+  else
+    prompt "The Computer wins the match!"
+  end
 end
 
 def empty_squares(brd)
@@ -51,7 +69,7 @@ end
 def player_move!(board)
   square = ""
   loop do
-    prompt "Choose a square (#{empty_squares(board).join(", ")})"
+    prompt "Choose a square (#{joinor(empty_squares(board))})"
     square = gets.chomp.to_i
     break if empty_squares(board).include?(square)
     prompt "invalid play"
@@ -73,18 +91,10 @@ def someone_won?(board)
 end
 
 def detect_winner(board)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-                  [[1, 5, 9], [3, 5, 7]]
-
-  winning_lines.each do |line|
-    if board[line[0]] == PLAYER_MARKER &&
-       board[line[1]] == PLAYER_MARKER &&
-       board[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if board.values_at(*line).count(PLAYER_MARKER) == 3
       return "Player"
-    elsif board[line[0]] == COMPUTER_MARKER &&
-          board[line[1]] == COMPUTER_MARKER &&
-          board[line[2]] == COMPUTER_MARKER
+    elsif board.values_at(*line).count(COMPUTER_MARKER) == 3
       return "Computer"
     end
   end
@@ -92,23 +102,35 @@ def detect_winner(board)
 end
 
 loop do
-  board = initialize_board
-
+  comp_score = 0
+  player_score = 0
   loop do
-    display_board(board)
-    player_move!(board)
-    break if someone_won?(board) || board_full?(board)
-    computer_move!(board)
-    display_board(board)
-    break if someone_won?(board) || board_full?(board)
-  end
+    board = initialize_board
 
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
+    loop do
+      display_board(board)
+      display_score(player_score, comp_score)
+      player_move!(board)
+      break if someone_won?(board) || board_full?(board)
+      computer_move!(board)
+      display_board(board)
+      break if someone_won?(board) || board_full?(board)
+    end
 
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won this round"
+      if detect_winner(board) == "Player"
+        player_score += 1
+      elsif detect_winner(board) == "Computer"
+        comp_score += 1
+      end
+    else
+      prompt "Round ends in a tie"
+    end
+
+    break if comp_score == 5 || player_score == 5
+  end
+  display_match_winner(player_score, comp_score)
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
