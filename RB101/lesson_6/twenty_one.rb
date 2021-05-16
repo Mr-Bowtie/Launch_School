013e86bbffeea7ffe2a6a4594699707c8160970f
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-05-16 04:06:43 -0500

    refactored to hash lookups for player info

--- RB101/lesson_6/twenty_one.rb
+++ RB101/lesson_6/twenty_one.rb
@@ -1,5 +1,6 @@
 require "pry"
 CARD_VALUES = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10, "Jack" => 10, "Queen" => 10, "King" => 10, "Ace" => 11 }
+TARGET_WINS = 5
 
 def prompt(input)
   puts "==|: #{input}"
@@ -13,38 +14,48 @@ def initialize_deck
     end
     new_deck << "Jack" << "Queen" << "King" << "Ace"
   end
-  new_deck
+  new_deck.shuffle
 end
 
-def display_hands(player, dealer, state = nil)
+def initialize_players
+  return { player: [], dealer: [] }, { player: 0, dealer: 0 }
+end
+
+def display_hands(hands, totals, state = nil)
   if state == "final"
     puts "**********************************"
-    prompt "Dealer has: #{dealer.join(", ")} ==> #{eval_hand(dealer)}"
-    prompt "You have: #{player.join(", ")} ==> #{eval_hand(player)}"
+    prompt "Dealer has: #{hands[:dealer].join(", ")} ==> #{eval_hand!(hands, :dealer, totals)}"
+    prompt "You have: #{hands[:player].join(", ")} ==> #{eval_hand!(hands, :player, totals)}"
     puts "**********************************"
   else
-    prompt "Dealer has: #{dealer[0]} and unknown card"
-    prompt "You have: #{player.join(", ")}"
+    prompt "Dealer has: #{hands[:dealer][0]} and unknown card"
+    prompt "You have: #{hands[:player].join(", ")}"
   end
 end
 
-def deal!(player, deck)
-  player << deck.delete_at(deck.index(deck.sample))
+def deal!(deck, hands, player)
+  hands[player] << deck.shift
 end
 
-def busted?(hand_total)
-  hand_total > 21
+def eval_hand!(hands, player, totals)
+  number_of_aces = hands[player].count("Ace")
+  sum = hands[player].map { |card| CARD_VALUES[card] }.sum
+  number_of_aces.times { sum -= 10 if (sum > 21) }
+  totals[player] = sum
+end
+
+def busted?(totals, player)
+  totals[player] > 21
 end
 
 def play_again?
-  positive_answers = ["yes", "y"]
-  negative_answers = ["no", "n"]
   loop do
     prompt "would you like to play another hand? (y/n)"
     answer = gets.chomp.downcase
-    if positive_answers.include?(answer)
+    case answer
+    when "yes", "y"
       return true
-    elsif negative_answers.include?(answer)
+    when "no", "n"
       return false
     else
       prompt "It's really a yes or no question, try again"
@@ -52,33 +63,20 @@ def play_again?
   end
 end
 
-def eval_hand(hand)
-  number_of_aces = hand.count("Ace")
-  sum = hand.map { |card| CARD_VALUES[card] }.sum
-  number_of_aces.times { sum -= 10 if (sum > 21) }
-  sum
-end
-
-def display_grand_output(player, player_total, dealer, dealer_total, state = nil)
-  system "clear"
-  display_hands(player, dealer, state)
-  display_result(player_total, dealer_total)
-end
-
-def calc_result(player, dealer)
-  if player > 21
+def calc_result(totals)
+  if busted?(totals, :player)
     :player_busted
-  elsif dealer > 21
+  elsif busted?(totals, :dealer)
     :dealer_busted
-  elsif player > dealer
+  elsif totals[:player] > totals[:dealer]
     :player
-  elsif dealer > player
+  elsif totals[:dealer] > totals[:player]
     :dealer
   end
 end
 
-def display_result(player, dealer)
-  case calc_result(player, dealer)
+def display_result(totals)
+  case calc_result(totals)
   when :player_busted
     prompt "You busted, dealer wins"
   when :dealer_busted
@@ -92,64 +90,87 @@ def display_result(player, dealer)
   end
 end
 
+def display_grand_output(hands, totals)
+  system "clear"
+  display_hands(hands, totals, "final")
+  display_result(totals)
+end
+
+def update_hands_won(totals, hands_won)
+  case calc_result(totals)
+  when :dealer_busted, :player
+    hands_won[:player] += 1
+  when :player_busted, :dealer
+    hands_won[:dealer] += 1
+  end
+end
+
+def grand_winner(hands_won)
+  hands_won[:player] == TARGET_WINS ? :player : dealer
+end
+
+def target_reached(hands_won)
+  hands_won.values.include?(5)
+end
+
 # TODO add in friendly messages and clear directions
 
 loop do
-  deck = []
-  player_hand = []
-  dealer_hand = []
-  system "clear"
-  deck = initialize_deck
-  2.times do
-    deal!(player_hand, deck)
-    deal!(dealer_hand, deck)
-  end
-  player_total = eval_hand(player_hand)
-  dealer_total = eval_hand(dealer_hand)
-
-  # * Players turn
-
+  hands_won = { player: 0, dealer: 0 }
   loop do
-    display_hands(player_hand, dealer_hand)
-    puts ""
-    prompt "(H)it or (S)tay?"
-    action = gets.chomp.downcase
-    if %(hit, h).include?(action)
-      system "clear"
-      deal!(player_hand, deck)
-      player_total = eval_hand(player_hand)
-    elsif !%(hit, stay, h, s).include?(action)
-      prompt "Invalid input: Either Hit or Stay"
+    hands, totals = initialize_players
+    deck = initialize_deck
+    system "clear"
+    2.times do
+      deal!(deck, hands, :player)
+      deal!(deck, hands, :dealer)
+    end
+    eval_hand!(hands, :player, totals)
+    eval_hand!(hands, :dealer, totals)
+
+    # * Players turn
+
+    loop do
+      display_hands(hands, totals)
+      puts ""
+      prompt "(H)it or (S)tay?"
+      action = gets.chomp.downcase
+      if %(hit, h).include?(action)
+        system "clear"
+        deal!(deck, hands, :player)
+        eval_hand!(hands, :player, totals)
+      elsif !%(hit, stay, h, s).include?(action)
+        prompt "Invalid input: Either Hit or Stay"
+        next
+      end
+      break if %(stay, s).include?(action) || busted?(totals, :player)
+    end
+
+    if busted?(totals, :player)
+      display_grand_output(hands, totals)
       next
     end
-    # binding.pry
-    break if %(stay, s).include?(action) || busted?(player_total)
+
+    # * Dealers turn
+
+    loop do
+      break if totals[:dealer] >= 17
+      deal!(deck, hands, :dealer)
+      eval_hand!(hands, :dealer, totals)
+    end
+
+    if busted?(totals, :dealer)
+      display_grand_output(hands, totals)
+      next
+    end
+
+    display_grand_output(hands, totals)
+    update_hands_won(totals, hands_won)
+    sleep(2)
+    break if target_reached(hands_won)
   end
 
-  if busted?(player_total)
-    display_grand_output(player_hand, player_total, dealer_hand, dealer_total, "final")
-    play_again?() ? next : break
-    next
-  end
-
-  # * Dealers turn
-
-  loop do
-    break if dealer_total >= 17
-    deal!(dealer_hand, deck)
-    dealer_total = eval_hand(dealer_hand)
-  end
-
-  if busted?(dealer_total)
-    display_grand_output(player_hand, player_total, dealer_hand, dealer_total, "final")
-    play_again?() ? next : break
-    next
-  end
-
-  display_grand_output(player_hand, player_total, dealer_hand, dealer_total, "final")
   break unless play_again?()
 end
-
 prompt "Thanks for playing 21, Goodbye!"
-
 # ? maybe add a betting/scoring system tracked from round to round
