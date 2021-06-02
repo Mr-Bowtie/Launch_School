bedeb76391778c8eacd6af71032d9cd13601e45b
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-06-02 01:44:57 -0500

    further exploration

--- exercises/small_problems/easy_9/counting_up.rb
+++ exercises/small_problems/easy_9/counting_up.rb
@@ -10,11 +10,14 @@
 #   - return int_sequence
 
 def sequence(int)
-  int_sequence = []
-  1.upto(int) { |i| int_sequence << i }
-  int_sequence
+  if int.negative?
+    return (int..1).to_a
+  else
+    return (1..int).to_a
+  end
 end
 
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+p sequence(-3) == [-3, -2, -1, 0, 1]
